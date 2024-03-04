b86857fc9bd12f4804010f9c53bd09cec02dd243
AlanAndreUP <alanenmexico12@gmail.com>
2024-03-03 22:21:53 -0600

    04

--- src/api/graphql/resolvers/Mutation.ts
+++ src/api/graphql/resolvers/Mutation.ts
@@ -8,7 +8,14 @@ interface RegisterUserArgs {
     password: string;
   };
 }
-
+interface UpdateUserArgs {
+  id: string;
+  update: {
+    name?: string;
+    email?: string;
+    // agrega lo demas papu
+  };
+}
 
 interface AuthenticateParams {
     email: string;
@@ -22,6 +29,24 @@ const Mutation = {
   loginUser: async (_: any, { email, password }: AuthenticateParams) => {
     return userService.authenticateUser({email,password});
   },
+  updateUser: async (parent: any, { id, name, email }: any, context: any) => {
+    if (!context.user) {
+      throw new Error("Autenticación requerida.");
+    }
+ 
+    return userService.updateUser({ id, update: { name, email } });
+  },
+  
+  changeUserPassword: async (_: any, { userId, oldPassword, newPassword }: { userId: string, oldPassword: string, newPassword: string }) => {
+    return userService.changePassword({userId, oldPassword, newPassword});
+  },
+  changeUserEmail: async (_: any, { userId, oldEmail, newEmail }: { userId: string, oldEmail: string, newEmail: string }) => {
+    return userService.emailPassword({userId, oldEmail, newEmail});
+  },
+  deleteUser: async (_: any, { userId }: { userId: string }) => {
+    return userService.deleteUser(userId);
+  },
+  
 
 };
 
--- src/api/graphql/schemas/index.ts
+++ src/api/graphql/schemas/index.ts
@@ -10,29 +10,37 @@ const typeDefs = gql`
     password: String
     role: String
   }
+
   type UsersPage {
     users: [User]
     totalPages: Int
     currentPage: Int
   }
-  
 
   type Query {
     users(page: Int, limit: Int): UsersPage
     user(id: ID!): User
+    searchUsers(searchTerm: String!): [User]
+    usersWithFilters(role: String!): [User]
+    totalUserCount: Int
   }
-  
 
   type Mutation {
     registerUser(user: RegisterUserInput!): User
     loginUser(email: String!, password: String!): String # JWT token
+    updateUser(id: ID!, name: String, email: String): User
+    changeUserPassword(userId: ID!, oldPassword: String!, newPassword: String!): Boolean
+    changeUserEmail(userId: ID!, oldEmail: String!, newEmail: String!): Boolean
+    deleteUser(userId: ID!): Boolean
   }
-  
+
   input RegisterUserInput {
     name: String!
     email: String!
     password: String!
   }
+
+  
 `;
 
 export default typeDefs;
